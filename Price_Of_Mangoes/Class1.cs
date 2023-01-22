cd886486a4e0f1aefff5916a91cab7718568ca36
GoodGod <guskov.workakk@gmail.com>
2023-01-22 03:04:41 +0300

    Price_Of_Mangoes done

---
+++ Price_Of_Mangoes/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace Price_Of_Mangoes;
+
+public static class Class1
+{
+    public static int GetPriceForMango(int count, int price)
+    {
+        var freeMangoCount = count / 3;
+        return (count - freeMangoCount) * price;
+    }
+}
