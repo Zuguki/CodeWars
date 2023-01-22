cf6a9c6d9b4a1d2d31aba7a48b9e39594840afbc
GoodGod <guskov.workakk@gmail.com>
2023-01-23 02:02:36 +0300

    DublicateEncoder done

---
+++ Duplicate_Encoder/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+namespace Duplicate_Encoder;
+
+public class Kata
+{
+    public static string DuplicateEncode(string word)
+    {
+        var wordInLower = word.ToLower();
+        var result = new StringBuilder();
+        var lettersCount = new Dictionary<char, int>();
+
+        foreach (var letter in wordInLower)
+        {
+            if (!lettersCount.ContainsKey(letter))
+                lettersCount.Add(letter, 0);
+            
+            lettersCount[letter]++;
+        }
+
+        foreach (var letter in wordInLower)
+            result.Append(lettersCount[letter] == 1 ? "(" : ")");
+
+        return result.ToString();
+    }
+}
