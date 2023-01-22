501cfde7129e8d11694bf64ddc5e5ed7cb18742e
GoodGod <guskov.workakk@gmail.com>
2023-01-22 03:04:56 +0300

    Unique_In_Order

---
+++ Unique_In_Order/Class1.cs
@@ -0,0 +1,11 @@
+﻿namespace Unique_In_Order;
+ 
+ public static class Class1
+ {
+     public static IEnumerable<T> UniqueInOrder<T>(IEnumerable<T> iterable)
+     {
+         var enumerable = iterable as T[] ?? iterable.ToArray();
+         return enumerable
+             .Where((item, index) => index == 0 || !item!.Equals(enumerable[index - 1]));
+     }
+ }
