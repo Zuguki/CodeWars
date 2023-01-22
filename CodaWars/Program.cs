6549d3e5a8845b81bfce4064ed52032da162b6ab
GoodGod <guskov.workakk@gmail.com>
2023-01-22 03:04:16 +0300

    Add Solution

---
+++ CodaWars/Program.cs
@@ -0,0 +1,11 @@
+﻿using Unique_In_Order;
+
+PrintCollection(Class1.UniqueInOrder("AAAABBBCCDAABBB"));
+
+static void PrintCollection<T>(IEnumerable<T> collection)
+{
+    foreach (var item in collection)
+        Console.Write(item);
+
+    Console.WriteLine();
+}
