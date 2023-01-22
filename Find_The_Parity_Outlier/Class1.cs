abbfb0ffd5544d5f57e2af0fc40708a7ff011fc4
GoodGod <guskov.workakk@gmail.com>
2023-01-23 01:51:35 +0300

    FindThePartyOutlier done

---
+++ Find_The_Parity_Outlier/Class1.cs
@@ -0,0 +1,23 @@
+﻿namespace Find_The_Parity_Outlier;
+
+public class Kata
+{
+    public static int Find(int[] integers)
+    {
+        var oddNumber = 0;
+        var evenNumber = 0;
+        var evenNumberCounter = 0;
+        foreach (var item in integers)
+        {
+            if (item % 2 == 0)
+            {
+                evenNumber = item;
+                evenNumberCounter++;
+            }
+            else
+                oddNumber = item;
+        }
+
+        return evenNumberCounter == 1 ? evenNumber : oddNumber;
+    }
+}
