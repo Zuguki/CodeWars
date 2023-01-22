c54d3be402d4b906f4dae0be1fcb93a09d45b6ce
GoodGod <guskov.workakk@gmail.com>
2023-01-22 03:31:14 +0300

    Whi_Likes_It done

---
+++ Who_Likes_It/Class1.cs
@@ -0,0 +1,14 @@
+﻿namespace Who_Likes_It;
+
+public static class Kata
+{
+    public static string Likes(string[] names) =>
+        names.Length switch
+        {
+            0 => "no one likes this",
+            1 => $"{names[0]} likes this",
+            2 => $"{names[0]} and {names[1]} like this",
+            3 => $"{names[0]}, {names[1]} and {names[2]} like this",
+            _ => $"{names[0]}, {names[1]} and {names.Length - 2} others like this"
+        };
+}
