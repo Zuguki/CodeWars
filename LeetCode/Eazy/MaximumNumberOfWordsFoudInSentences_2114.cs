fd320fa16423c3587d5847d193813fbfeef71586
Zuguki <guskov.zahar20@gmail.com>
2023-03-28 01:21:04 +0300

    2114 done

---
+++ LeetCode/Eazy/MaximumNumberOfWordsFoudInSentences_2114.cs
@@ -0,0 +1,8 @@
+namespace LeetCode.Eazy;
+
+public class MaximumNumberOfWordsFoudInSentences_2114
+{
+    public int MostWordsFound(string[] sentences) =>
+        sentences.Select(str => str.Split(' ').Length)
+            .Max();
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,1 +1,6 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using LeetCode.Eazy;
+
+var instance = new MaximumNumberOfWordsFoudInSentences_2114();
+
+var sentance = new[] {"alice and bob love leetcode", "i think so too", "this is great thanks very much"};
+Console.WriteLine(instance.MostWordsFound(sentance));
