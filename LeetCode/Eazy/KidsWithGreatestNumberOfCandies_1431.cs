206c5739c8c7c0858642a04f72128efec1021a73
Zuguki <guskov.zahar20@gmail.com>
2023-04-17 13:05:15 +0300

    Updated

--- LeetCode/Eazy/KidsWithGreatestNumberOfCandies_1431.cs
+++ LeetCode/Eazy/KidsWithGreatestNumberOfCandies_1431.cs
@@ -7,11 +7,7 @@ public class KidsWithGreatestNumberOfCandies_1431
 {
     public IList<bool> KidsWithCandies(int[] candies, int extraCandies)
     {
-        var maxCandy = candies.Max();
-        var result = new List<bool>(candies.Length);
-        foreach (var candy in candies)
-            result.Add((candy + extraCandies) >= maxCandy);
-
-        return result;
+        var mostBeGreaterThan = candies.Max() - extraCandies;
+        return candies.Select(candy => candy >= mostBeGreaterThan).ToList();
     }
 }
